82825bdfdd3fbf23954ee325e98542b51d858eec
IsabellaCane <ISABELLACANE@ISABELLAs-MacBook-Pro.local>
2017-06-03 22:54:11 +1000

    -

--- P3/articles/assets/js/main.js
+++ P3/articles/assets/js/main.js
@@ -10,6 +10,30 @@ $(document).ready(function() {
       $('.title').removeClass('showme');
     });
 
+    $('.col__2of4').mouseenter(function() {
+      $('.title2').addClass('showme');
+    });
+
+    $('.col__2of4').mouseleave(function() {
+      $('.title2').removeClass('showme');
+    });
+
+    $('.col__3of4').mouseenter(function() {
+      $('.title3').addClass('showme');
+    });
+
+    $('.col__3of4').mouseleave(function() {
+      $('.title3').removeClass('showme');
+    });
+
+    $('.col__4of4').mouseenter(function() {
+      $('.title4').addClass('showme');
+    });
+
+    $('.col__4of4').mouseleave(function() {
+      $('.title4').removeClass('showme');
+    });
+
 
 
 
